e1e36a1e12f23eec874dfae08151b645ba9941c3
ELBEBACK <elbekbegaliev305@gmail.com>
2026-04-15 00:48:56 +0300

    updated sample + few refinements

--- src/parse/parser.cpp
+++ src/parse/parser.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include "parser.hpp"
 
 
@@ -8,12 +10,14 @@ parsed_input read(std::istream& in, uint32_t edge_count, uint32_t def_count) {
 
     parsed_input result;
 
+    std::cout << "Enter your edges: <src> <dst>" << std::endl;
     for (uint32_t i = 0; i < edge_count; ++i) {
         uint32_t src, dst;
         in >> src >> dst;
         result.cfg.edge_add(src, dst);
     }
 
+    std::cout << "Enter your defs: <blocks>" << std::endl;
     for (uint32_t i = 0; i < def_count; ++i) {
         uint32_t d;
         in >> d;
